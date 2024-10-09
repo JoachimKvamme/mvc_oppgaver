6509c9ef9c2c04ef7c972167c0de8022d972434a
JoachimKvamme <kaj082024@gmail.com>
2024-10-09 10:16:42 +0200

    Nå er det mulig å oppdatere egenskapene til Book-elementer i Model, gjennom Run()

--- BookController.cs
+++ BookController.cs
@@ -9,10 +9,24 @@ public class Controller {
         _model = model;
         _view = view;
     }
+
+    // Update-metodene under tar book-id som parameter, og lar brukeren endre den valgte egenskapen.
     public void UpdateTitle(int id) {
         Console.WriteLine($"Please choose a new title for the book with the title {_model.Books[id].Title}:");
         string? newTitle = Console.ReadLine();
         _model.Books[id].Title = newTitle;
+    }
+     public void UpdateAuthor(int id) {
+        Console.WriteLine($"Please choose a new author for the book with the title {_model.Books[id].Title}:");
+        string? newAuthor = Console.ReadLine();
+        _model.Books[id].Author = newAuthor;
+
+    }
+    public void UpdateYear(int id) {
+        Console.WriteLine($"Please choose a new release year for the book with the title {_model.Books[id].Title}:");
+        int newYear = int.Parse(Console.ReadLine());
+        _model.Books[id].Year = newYear;
+
     }
 
     public void AddBook(Book book) {
@@ -21,12 +35,7 @@ public class Controller {
     public void RemoveBook(int id) {
         _model.Books.RemoveAt(id);
     }
-    public void UpdateAuthor(int id) {
-
-    }
-    public void UpdateYear(int id) {
-
-    }
+   
 
 
     public void DisplayBooks() {
@@ -46,12 +55,28 @@ public class Controller {
             Console.WriteLine($"You have choosen to update the book {_model.Books[bookId-1].Title} with the Id {bookId}, released {_model.Books[bookId-1].Year} and written by {_model.Books[bookId-1].Author}");
             Console.WriteLine($"Do you wish to update the title, the year or the author? Please type 'title', 'year' or 'author'");
             string updateChoice = Console.ReadLine();
-            
+            switch (updateChoice) {
+                case "title":
+                UpdateTitle(bookId-1);
+                DisplayBooks();
+                break;
+                case "year":
+                UpdateYear(bookId-1);
+                break;
+                case "author":
+                UpdateAuthor(bookId);
+                break;
+                default:
+                break;
+            }
             
             break;
             case "remove":
             break;
             case "add":
+            break;
+            case "view":
+            DisplayBooks();
             break;
             default:
             Console.WriteLine("You must choose to update, remove og add, with the correct prompts.");
